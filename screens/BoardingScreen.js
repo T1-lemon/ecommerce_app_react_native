223a116126c22bf44f92550d3cb3477ec7602873
HuongHuong <huongghuongg9900@gmail.com>
2022-12-19 21:43:36 +0700

    Huong test commit

--- screens/BoardingScreen.js
+++ screens/BoardingScreen.js
@@ -14,6 +14,7 @@ export default function BoardingScreen(props) {
         title="abc"
         onPress={() => navigation.navigate("CartScreen")}
       ></Button>
+      <Text>Huong Huong </Text>
     </View>
   );
 }
